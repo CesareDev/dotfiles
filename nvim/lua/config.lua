7f27ba99a78f69c8a7cc649e582f3f9175e3d5dd
CesareDev <corsicesare.lavoro@gmail.com>
2026-03-26 19:13:42 +0100

    Change neovim keybindigs

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -2,24 +2,25 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-
 vim.cmd("set clipboard+=unnamedplus")
-
 vim.g.mapleader = ","
-
--- Show relative line numbers
 vim.opt.relativenumber = true
-
 -- vim.cmd('set signcolumn=auto:2')
 vim.cmd('set signcolumn=number')
 -- vim.opt.wrap = false
 
-vim.keymap.set('n', '.', ':', {})
+vim.diagnostic.config({
+    virtual_lines = false,
+    virtual_text = true
+})
 
-vim.keymap.set('n', '<M-Up>', '<Up><End>Vxp<Up>V=', {})
-vim.keymap.set('n', '<M-Down>', '<End>VxpV=', {})
-vim.keymap.set('n', '<leader>l', 'Vy<End>p<End>', {})
+vim.keymap.set('n', '.', ':', {})
+vim.keymap.set('n', '<M-Up>', ':m .-2==', {})
+vim.keymap.set('n', '<M-Down>', ':m .+1==', {})
+vim.keymap.set('n', '<leader>l', 'yyp', {})
 vim.keymap.set('v', '<BS>', '"_d', {})
+vim.keymap.set('n', '<leader>t', ':terminal<CR>i', {})
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {})
 
 local function searchAndReplace()
     -- Prompt the user for the word to search and the replacement word
@@ -34,8 +35,6 @@ local function searchAndReplace()
     vim.cmd("%s@" .. search_word .. "@" .. replace_word .. "@gc")
 end
 vim.keymap.set('n', '<leader>r', searchAndReplace, {})
-
-vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
 
 local function switch_right()
     vim.cmd('bnext')
@@ -59,5 +58,3 @@ end
 vim.keymap.set({ 'n', 't' }, '<M-Right>', switch_right, {})
 vim.keymap.set({ 'n', 't' }, '<M-Left>', switch_left, {})
 
-vim.keymap.set('n', '<leader>t', ':terminal<CR>i', {})
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {})
